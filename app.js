6a1ca7c2c091b1208e1d9d6aeb81cd494a9d6e97
Igor Goral <wigurs33@gmail.com>
2026-04-14 10:46:45 +0200

    feat: dodano oznaczanie zadan jako ukonczonych

--- app.js
+++ app.js
@@ -7,7 +7,27 @@ function dodaj(event) {
     if (textToAdd.trim() === "") return;
 
     const li = document.createElement("li");
-    li.textContent = textToAdd;
+
+    // tworzymy checkbox
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    // reakcja na kliknięcie checkboxa
+    checkbox.addEventListener("change", function () {
+        if (checkbox.checked) {
+            li.classList.add("done");
+        } else {
+            li.classList.remove("done");
+        }
+    });
+
+    // tekst zadania
+    const span = document.createElement("span");
+    span.textContent = textToAdd;
+
+    // składamy element
+    li.appendChild(checkbox);
+    li.appendChild(span);
 
     document.getElementById("task-list").appendChild(li);
 
